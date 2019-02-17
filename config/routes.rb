2a816b3ae5b70d0c3be481fa14bd98704fbb97fd
onnakodomo <onnakodomo0618@gmail.com>
2019-02-18 01:16:18 +0900

    グループ新規作成画面(ルート構築迄)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root                      'messages#index'
-  get   'messages'       => 'messages#index'
-  get   'users/:id/edit' => 'users#edit'
-  patch 'users/:id'      => 'user#update'
+  resources :messages, only: [:index]
+  resources :users,    only: [:edit, :update]
+  resources :groups,   only: [:new, :create]
 end

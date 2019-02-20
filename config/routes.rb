cc785f50954191c27777e58720f401cbc8248a88
onnakodomo <onnakodomo0618@gmail.com>
2019-02-20 19:55:30 +0900

    新規グループ作成機能と編集機能実装(修正1)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root                      'groups#index'
   resources :users,    only: [:edit, :update]
-  resources :groups do
-    resources :messages
-  end
+  resources :groups,   only: [:new, :create, :edit, :update]
 end
+

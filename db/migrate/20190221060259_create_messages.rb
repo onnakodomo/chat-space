a40d02af92217aef5103d097c7039c139bab5264
onnakodomo <onnakodomo0618@gmail.com>
2019-02-21 16:29:59 +0900

    メッセージ投稿機能(MVC構築、モデル、テーブル作成迄)

--- db/migrate/20190221060259_create_messages.rb
+++ db/migrate/20190221060259_create_messages.rb
@@ -1,9 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text
-      t.references
-      t.referrence
+      t.text                    :body
+      t.text                    :image
+      t.references              :user,    null:false,foreign_key:true
+      t.references              :group,   null:false,foreign_key:true
       t.timestamps
     end
   end

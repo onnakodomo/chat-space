c413ac049692bee7c2353ecbaed579e49581ed5f
onnakodomo <onnakodomo0618@gmail.com>
2019-02-24 16:44:35 +0900

    sidebarの細かな修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,25 +3,14 @@ class Group < ApplicationRecord
   has_many  :users, through: :group_users
   has_many  :messages
   validates :name, presence: true
+
   def show_last_message
-
-  #   if (last_message = messages.last).present?
-  #     last_message.body? last_message.body :  '画像が投稿されています'
-  #   else
-  #     'まだメッセージはありません。'
-  #   end
-  # end
-
     if (last_message = messages.last).present?
-      if last_message.body?
-       last_message.body
-      else
-        '画像が投稿されています'
-      end
+      last_message.body? ? last_message.body :  '画像が投稿されています'
     else
       'まだメッセージはありません。'
     end
   end
 
-
 end
+

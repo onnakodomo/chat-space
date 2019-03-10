e457c00473bfd4e98d5abebf1d2e473a4bde7497
onnakodomo <onnakodomo0618@gmail.com>
2019-03-10 21:34:57 +0900

    簡単な修正(改行等)

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,16 +2,18 @@ $(function(){
 
 
   var searchresult = $("#user-search-result")
+  var addmemberHTML = $(".chat-group-form__field--right_bottom")
 
 
 // 検索結果の表示
   function searchMemberToHTML(member){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name"> ${ member.name } </p>
-                  <a href="" class="chat-group-user__btn--add" data-user-id="${ member.id }" data-user-name=" ${member.name } ">追加</a>
+                  <a href="" class="chat-group-user__btn--add" data-user-id="${ member.id }" data-user-name= "${member.name }"" >追加</a>
                 </div>`
-    searchresult.append(html);;
+    searchresult.append(html);
   }
+
 
   // 該当するメンバーなし
   function noMemberToHTML(msg){
@@ -46,10 +48,32 @@ $(function(){
     })
   }
 
+
+
   // 検索結果を削除
   function deleteSearchResult(){
     $("#user-search-result").empty();
   }
+
+
+  // 追加ボタンを押した後、HTMLを消去
+  function deleteAddMemberAfterClick(){
+      $("chat-group-user").empty();
+  }
+
+  // 追加するメンバーを「チャットメンバーの横に配置」
+  function addMemberToChatMemberHTML( addUserId, addUserName ){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value=' ${ addUserId }'>
+                  <p class='chat-group-user__name'>${ addUserName }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+                console.log(addUserId);
+                console.log(addUserName );
+    addmemberHTML.append(html);
+  }
+
+
 
 
   // イベント発火地点
@@ -63,4 +87,14 @@ $(function(){
       deleteSearchResult();
     }
   });
+
+  // 「追加」ボタンを押した時
+  $(document).on("click",".chat-group-user__btn--add",function(){
+    var addUserId = $(".chat-group-user__btn--add").attr("data-user-id");
+    var addUserName = $(".chat-group-user__btn--add").attr("data-user-name");
+    deleteAddMemberAfterClick();
+    addMemberToChatMemberHTML( addUserId, addUserName );
+  });
 });
+
+

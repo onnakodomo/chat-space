30502c46ba007a8224e5a8469e87abd0ac7b822c
onnakodomo <onnakodomo0618@gmail.com>
2019-03-19 20:21:39 +0900

    config修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,6 +5,7 @@ worker_processes 1
 working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 listen "#{app_path}/tmp/sockets/unicorn.sock"
+#エラーをlog/unicorn.stderr.logに記録するという指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
